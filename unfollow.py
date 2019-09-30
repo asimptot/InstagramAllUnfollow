05173e15a207c9774327c0fe4433e00c03c5317f
Asım <asimzorlu@gmail.com>
2019-09-30 11:13:37 +0300

    Add files via upload

---
+++ unfollow.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+import pyautogui
+import time
+
+def eminim():
+    pyautogui.click(926, 715) #emin misiniz'e yanit
+    time.sleep(5)
+
+def login():
+
+    pyautogui.click(1513, 214)
+    time.sleep(7)
+    pyautogui.click(857, 382)
+    pyautogui.typewrite('id') #enter your id
+    time.sleep(2)
+    pyautogui.click(905, 447)
+    pyautogui.typewrite('password') #enter your password
+    time.sleep(3)
+    pyautogui.click(938, 522)
+    time.sleep(5)
+
+def unf():
+
+    web_page_url = 'https://www.instagram.com/en_ucuz_ucus/'
+    driver_path = "D:\development_group\Tests\Instagram\chromedriver"
+    browser = webdriver.Chrome(executable_path=driver_path)
+    browser.maximize_window()
+    browser.get(web_page_url)
+    time.sleep(5)
+
+    login()
+
+    for i in range(90):
+        pyautogui.press('F5')
+        pyautogui.click(1115, 462) #takip listesi
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1129, 449) #unf
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1139, 519) #unf
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1128, 591) #unf
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1135, 662) #unf
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1147, 721) #unf
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1147, 793) #unf
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1147, 869) #unf
+        time.sleep(5)
+        eminim()
+
+        pyautogui.click(1214, 318) #close
+        time.sleep(3)
+unf()
+
+
+
