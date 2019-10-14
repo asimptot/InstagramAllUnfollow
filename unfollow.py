38e8a83e877899d1e91e647c588fcf1d937d006b
Asım <asimzorlu@gmail.com>
2019-10-14 17:20:57 +0300

    Add files via upload

--- unfollow.py
+++ unfollow.py
@@ -4,52 +4,55 @@ import time
 
 def eminim():
     pyautogui.click(926, 715) #emin misiniz'e yanit
-    time.sleep(2)
+    time.sleep(3)
 
 def login():
 
     pyautogui.click(1513, 214)
     time.sleep(4)
     pyautogui.click(857, 382)
-    pyautogui.typewrite('en_ucuz_ucus') #enter your id
+    pyautogui.typewrite('hotelizm') #enter your id
     time.sleep(2)
     pyautogui.click(905, 447)
-    pyautogui.typewrite('10031144') #enter your password
+    pyautogui.typewrite('zapzulukum38') #enter your password
     time.sleep(3)
     pyautogui.click(938, 522)
     time.sleep(5)
 
+def open_wo_login():
+    pyautogui.click(333, 1051)
+
 def unf():
 
-    web_page_url = 'https://www.instagram.com/en_ucuz_ucus/'
-    driver_path = "D:\development_group\Tests\Sosyal\Instagram\chromedriver"
+    web_page_url = 'https://www.instagram.com/hotelizm/'
+    driver_path = "D:\Projects\Sosyal\Instagram\chromedriver"
     browser = webdriver.Chrome(executable_path=driver_path)
     browser.maximize_window()
-    browser.get(web_page_url)
+    #browser.get(web_page_url)
     time.sleep(5)
 
-    login()
+    #login()
+    open_wo_login()
 
     for i in range(90):
         pyautogui.click(134, 78) #refresh
         time.sleep(8)
-        pyautogui.click(1115, 462) #takip listesi
+        pyautogui.click(1079, 463) #takip listesi
+        time.sleep(5)
+
+        pyautogui.click(1148, 452) #unf
         time.sleep(3)
         eminim()
 
-        pyautogui.click(1129, 449) #unf
+        pyautogui.click(1148, 519) #unf
         time.sleep(3)
         eminim()
 
-        pyautogui.click(1139, 519) #unf
+        pyautogui.click(1148, 591) #unf
         time.sleep(3)
         eminim()
 
-        pyautogui.click(1128, 591) #unf
-        time.sleep(3)
-        eminim()
-
-        pyautogui.click(1135, 662) #unf
+        pyautogui.click(1148, 662) #unf
         time.sleep(3)
         eminim()
 
