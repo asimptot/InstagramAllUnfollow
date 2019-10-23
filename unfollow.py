32d44366cb4509b0a6b469b8fcde23a2cf40c638
Asım <asimzorlu@gmail.com>
2019-10-23 21:05:24 +0300

    Add files via upload
    Only use tabs to unfollow your profile.

--- unfollow.py
+++ unfollow.py
@@ -1,76 +1,47 @@
-from selenium import webdriver
-import pyautogui
+import pyautogui as pg
 import time
 
-def eminim():
-    pyautogui.click(926, 715) #emin misiniz'e yanit
-    time.sleep(3)
-
-def login():
-
-    pyautogui.click(1513, 214)
-    time.sleep(4)
-    pyautogui.click(857, 382)
-    pyautogui.typewrite('hotelizm') #enter your id
+def sure():
+    pg.press('tab')
     time.sleep(2)
-    pyautogui.click(905, 447)
-    pyautogui.typewrite('zapzulukum38') #enter your password
-    time.sleep(3)
-    pyautogui.click(938, 522)
+    pg.press('enter')
     time.sleep(5)
 
 def open_wo_login():
-    pyautogui.click(333, 1051)
+    pg.click(333, 752)
+    time.sleep(2)
 
-def unf():
 
-    web_page_url = 'https://www.instagram.com/hotelizm/'
-    driver_path = "D:\Projects\Sosyal\Instagram\chromedriver"
-    browser = webdriver.Chrome(executable_path=driver_path)
-    browser.maximize_window()
-    #browser.get(web_page_url)
+def refresh():
+    pg.click(86, 49)  # refresh
+    time.sleep(8)
+
+
+def follow_list():
+    for m in range(7):
+        pg.press('tab')
+        time.sleep(2)
+    pg.press('enter')
     time.sleep(5)
 
-    #login()
+
+def unfollow_all():
+    for j in range(0, 100, 4):
+        for k in range(7 + j):
+            pg.press('tab')
+            time.sleep(2)
+        pg.press('enter')
+        time.sleep(5)
+        sure()
+
+def main():
     open_wo_login()
+    refresh()
+    follow_list()  # takipçi listesi
+    unfollow_all()
 
-    for i in range(90):
-        pyautogui.click(134, 78) #refresh
-        time.sleep(8)
-        pyautogui.click(1079, 463) #takip listesi
-        time.sleep(5)
-
-        pyautogui.click(1148, 452) #unf
-        time.sleep(3)
-        eminim()
-
-        pyautogui.click(1148, 519) #unf
-        time.sleep(3)
-        eminim()
-
-        pyautogui.click(1148, 591) #unf
-        time.sleep(3)
-        eminim()
-
-        pyautogui.click(1148, 662) #unf
-        time.sleep(3)
-        eminim()
-
-        pyautogui.click(1147, 721) #unf
-        time.sleep(5)
-        eminim()
-
-        pyautogui.click(1147, 793) #unf
-        time.sleep(3)
-        eminim()
-
-        pyautogui.click(1147, 869) #unf
-        time.sleep(3)
-        eminim()
-
-        pyautogui.click(1214, 318) #close
-        time.sleep(3)
-unf()
+    pg.click(860, 238)  # close
+    time.sleep(3)
 
 
-
+main()
