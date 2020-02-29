4d23765258b6f79bc4e34a389fbaf40750826e17
Asım <asimzorlu@gmail.com>
2020-02-29 13:05:28 +0300

    Add files via upload

--- unfollow.py
+++ unfollow.py
@@ -5,50 +5,61 @@ def sure():
     pg.press('tab')
     time.sleep(0.5)
     pg.press('enter')
-    time.sleep(2)
+    time.sleep(1)
+
+def close_window():
+    pg.press('esc')
+    time.sleep(3)
 
 def open_wo_login():
-    pg.click(333, 752)
+    pg.press('win')
+    pg.typewrite('chrome.exe')
     time.sleep(2)
+    pg.press('enter')
+    time.sleep(2)
+    pg.typewrite('https://www.instagram.com/gizos_tekin/')
+    pg.press('enter')
+    time.sleep(2)
+    #pg.click(334, 1058)
+    #time.sleep(2)
 
 def refresh():
-    pg.click(86, 49)  # refresh
+    pg.click(127, 74)  # refresh
     time.sleep(8)
 
-
-def follow_list():
-    for m in range(7):
+def unfollow_list():
+    for m in range(6):
         pg.press('tab')
         time.sleep(0.5)
     pg.press('enter')
     time.sleep(5)
 
-
 def unfollow_all():
-    for j in range(0, 1000, 4):
+
+    unfollower = 20
+
+    for j in range(0, unfollower*4, 4):
         if j == 44:
             for m in range(99):
                 pg.press('tab')
                 time.sleep(0.25)
             pg.press('enter')
-            time.sleep(5)
+            time.sleep(1)
             sure()
         else:
             for k in range(7 + j):
                 pg.press('tab')
                 time.sleep(0.25)
             pg.press('enter')
-            time.sleep(5)
+            time.sleep(1)
             sure()
 
 def main():
     open_wo_login()
-    refresh()
-    follow_list()  # takipçi listesi
+    #refresh()
+    unfollow_list()  # takipçi listesi
     unfollow_all()
 
-    pg.click(860, 238)  # close
-    time.sleep(3)
-
+    close_window()
 
 main()
